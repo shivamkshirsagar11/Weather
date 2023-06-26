3cd9860948b92694eff51ed7f5a5183f8a3b041c
shivamkshirsagar11 <shivamkshirsagar2002@gmail.com>
2023-06-26 12:10:23 +0530

    location fetching from gps and ip complete

--- frontend/vatavaran/src/UI/Home/HomePage.jsx
+++ frontend/vatavaran/src/UI/Home/HomePage.jsx
@@ -1,35 +1,55 @@
-import React, {useState, useEffect} from 'react'
-import WeatherInfo from './WeatherInfo'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import { toast } from 'react-toastify';
-export default function HomePage(props) {
-    const [showGreetings, setShowGreetings] = useState(true);
+import LoadingResolve from '../Loading/LoadingResolve';
+export default function HomePage() {
     const [coords, setCoords] = useState({latitude:-1, longitude:-1});
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         toast.success("Welcome to Vatavaran Weather");
-        function CheckLocationAvailability(){
-            if(props.isGeolocationAvailable){
-                if(props.isGeolocationEnabled){
-                    if(props.coords){
-                        let tempCoords = {latitude: props.coords.latitude, longitude:props.coords.longitude}
-                        setCoords(tempCoords);
-                    }
-                    else{
-                        toast.error("No Co-ordinates Available, Please Enter Manuelly")
-                    }
-                }
-                else{
-                    toast.error("Location Access is not Enabled")
-                }
-            }
-            else{
-                toast.error("No Location Service is Available");
-            }
+        function getLocation(){
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(onSuccess, onError);
+              } else {
+                toast.error("Location Not Supported");
+              }
         }
-    CheckLocationAvailability();
+        function onSuccess(pos){
+            let tempLoc = {latitude:pos.coords.latitude,longitude:pos.coords.longitude};
+            setCoords(tempLoc);
+            setLoading(false);
+        }
+        function onError(){
+            toast.error("Error Getting location from GPS");
+            toast.info("Getting location from IP");
+            fromIP();
+        }
+        async function fromIP(){
+            const fetchIPAddressAndLoc = async () => {
+                try {
+                  const response1 = await axios.get('https://api.ipify.org?format=json');
+                  const { ip } = response1.data;
+                  const response = await axios.get(`http://ip-api.com/json/${ip}`);
+                    const { lat, lon } = response.data;
+                    setCoords({latitude:lat,longitude:lon});
+                } catch (error) {
+                  toast.error('Error fetching IP address');
+                  toast.error("Please Enter Location Manually, Error getting Location");
+                }
+                setLoading(false);
+              };
+              await fetchIPAddressAndLoc();
+        }
+        getLocation();
     }, []);
   return (
     <>
-     {/* {showGreetings && <WeatherInfo/>} */}
+    {loading && <LoadingResolve/>}
+    {!loading &&<>
+     latitude: {coords.latitude}<hr/>
+     longitude: {coords.longitude}<hr/>
+     </>
+    }
     <div>
         <h1>
             Vatavaran Weather app
---
+++ frontend/vatavaran/src/UI/Loading/LoadingResolve.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoadingResolve() {
+  return (
+    <div>
+      <h4 style={{color:"red"}}>Loading</h4>
+    </div>
+  )
+}
